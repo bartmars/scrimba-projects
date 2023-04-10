ebd2e26c432b39cd567d3cddb56c9d73f779cd95
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-04-10 21:39:06 +0200

    update project

--- restaurant-ordering/index.js
+++ restaurant-ordering/index.js
@@ -2,11 +2,15 @@ import { menuArray } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 
 const cartArray = []
+let isCartEmpty = true
 let cartTotal = 0
 
 document.addEventListener('click', function(e) {
     if (e.target.dataset.addItem) {
         handleAddToCart(e.target.dataset.addItem)
+    }
+    else if (e.target.dataset.removeItem) {
+        handleRemoveFromCart(e.target.dataset.removeItem)
     }
 })
 
@@ -27,6 +31,15 @@ function handleAddToCart(btnAddItemId) {
     render()
 }
 
+function toggleCartVisibility () {
+    if (cartArray.length === 0) {
+        isCartEmpty = true
+    } else {
+        isCartEmpty = false
+    }
+    document.getElementById('card-items').classList.toggle('hidden')
+}
+
 function handleRemoveFromCart () {
 
 }
@@ -41,10 +54,10 @@ function getMenuHtml() {
                 <div id="item-emoji-${item.id}" class="item-emoji">${item.emoji}</div>
                 <div>
                     <p id="item-name-${item.id}" class="item-name">${item.name}</p>
-                    <p id="item-info-${item.id}" class="item-description">${item.ingredients}<p>
+                    <p id="item-info-${item.id}" class="text-gray">${item.ingredients}<p>
                     <p id="item-price-${item.id}" class="item-price">$${item.price}</p>
                 </div>
-                <button id="item-btn-${item.id}" class="item-btn" data-add-item="${item.id}">+</button>
+                <button id="item-btn-${item.id}" class="btn-item" data-add-item="${item.id}">+</button>
             </div>
         </div>`
     })
@@ -57,7 +70,10 @@ function getCartHtml() {
     cartArray.forEach(function(item) {    
         cartArrayHtml += `
             <div class="item-inner">
-                <p id="item-name-${item.id}" class="item-name">${item.name}</p>
+                <p id="item-name-${item.id}" class="item-name">
+                    ${item.name} 
+                    <button id="btn-remove-${item.uuid}" class="text-gray btn-remove" data-remove-item="${item.uuid}">remove</button>
+                </p>
                 <p id="item-price-${item.id}" class="item-price">$${item.price}</p>
             </div>
         </div>`
@@ -71,7 +87,8 @@ function getCartTotalHtml() {
     cartArray.forEach(function() {
         cartTotalHtml = `
         <div>
-            <p>Total price: $${cartTotal}</p>
+            <p>Total price:</p>
+            <p>${cartTotal}
         </div>`
     })
     return cartTotalHtml
