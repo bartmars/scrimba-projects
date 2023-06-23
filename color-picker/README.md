7dd67260e5a2c8f89eab337f06f467a8be1822e2
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-06-23 20:24:01 +0200

    update project

--- color-picker/README.md
+++ color-picker/README.md
@@ -10,10 +10,10 @@ This repository is being auto-deployed via Netlify.
 ## Requirements
 
 Requirements
-* Choose “seed color” with an <input type=“color” />
-* Choose color scheme mode in a <select> box
-* Clicking button makes request to The Color API to get a color scheme
-* Display the scheme colors and hex values on the page
+* [x] Choose “seed color” with an <input type=“color” />
+* [x] Choose color scheme mode in a <select> box
+* [x] Clicking button makes request to The Color API to get a color scheme
+* [x] Display the scheme colors and hex values on the page
 
 Stretch goal:
-* Click hex values to copy to clipboard
+* [x] Click hex values to copy to clipboard
--- color-picker/index.js
+++ color-picker/index.js
@@ -1,2 +1,78 @@
+const schemeNames = [
+    'monochrome',
+    'monochrome-dark',
+    'monochrome-light',
+    'analogic',
+    'complement',
+    'analogic-complement',
+    'triad',
+    'quad'
+]
 
+let hexValue = 'F55A5A'
+let schemeValue = 'monochrome'
 
+document.getElementById('color-picker').addEventListener('change', (e) => {
+    hexValue = document.getElementById('color-picker').value.substring(1)
+})
+
+document.getElementById('select-scheme').addEventListener('change', (e) => {
+    schemeValue = document.getElementById('select-scheme').value
+})
+
+document.addEventListener('click', (e) => {
+    for (let i = 0; i < 5; i++) {
+        if (e.target.id === `color-${i}`) {
+            copyToClipboard(document.getElementById(`label-${i}`).textContent)
+        }        
+    }
+})
+
+document.getElementById('get-scheme-btn').addEventListener('click', (e) => {
+    e.preventDefault()
+    getColorScheme(hexValue, schemeValue)
+})
+
+function getColorScheme(hex, mode) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}`)
+    .then(response => response.json())
+    .then(data => updateColors(data))
+}
+
+function updateColors(data) {
+    for (let i = 0; i < 5; i++) {            
+        document.getElementById(`color-${i}`).style.backgroundColor = data.colors[i].hex.value
+        document.getElementById(`label-${i}`).textContent = data.colors[i].hex.value
+    }
+}
+
+function copyToClipboard(color) {
+    navigator.clipboard.writeText(color)
+}
+
+function getColorSchemesSelectorHtml() {
+    let html = ``
+    schemeNames.forEach((item) => {
+        html += `   <option>${item}</option>`
+    })
+    return html
+}
+
+function getColorSchemeHtml() {
+    let html = ``
+    for (let i = 0; i < 5; i++) {
+        html += `
+            <div class="color-layout">
+                <div id="color-${i}" class="color"></div>
+                <p id="label-${i}"></p>
+            </div>`        
+    }
+    return html
+}
+
+function render() {
+    document.getElementById('select-scheme').innerHTML = getColorSchemesSelectorHtml()
+    document.getElementById('color-scheme').innerHTML = getColorSchemeHtml()
+}
+
+render()
