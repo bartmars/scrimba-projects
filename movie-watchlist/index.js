09ce923a4b80278a9e5e26a626a891e3150e9bb2
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-07-08 10:06:48 +0200

    update project

--- movie-watchlist/index.js
+++ movie-watchlist/index.js
@@ -3,41 +3,55 @@ const searchInput = document.getElementById('search-input')
 const movieResults = document.getElementById('movie-results')
 
 let searchTerm = ''
-let html
+let html = ''
+let movieArrayHtml = ''
+let movieArray = []
+
 
 searchInput.addEventListener('change', (e) => {
     searchTerm = searchInput.value
+    // searchInput.value = ""
     return searchTerm
 })
 
-searchBtn.addEventListener('click', (e) => {
+searchBtn.addEventListener('click', async (e) => {
     e.preventDefault()
     getMovie(searchTerm)
 })
 
-function getMovie(movieTitle) {
-    fetch(`http://www.omdbapi.com/?t=${movieTitle}&plot=full&apikey=ead28c58&t`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            movieResults.style.background = 'none'
-            getMovieListHtml(data.Poster, data.Title, data.imdbRating, data.Runtime, data.Genre, data.Plot)
-            renderHtml()
-    })
+async function getMovie(movieTitle) {
+    const movieResponse = await fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=ead28c58&t`)
+    const data = await movieResponse.json()
+    // console.log(data.Search[0])
+
+    for (let i = 0; i < data.Search.length; i++) {
+        const imdbIdResponse = await fetch(`http://www.omdbapi.com/?i=${data.Search[i].imdbID}&apikey=ead28c58&t`)
+        const imdbIdData = await imdbIdResponse.json()
+        // console.log(imdbIdData)
+        movieArray.push(imdbIdData)
+        // console.log(movieArray)
+        movieArray.forEach(item => {
+            movieArrayHtml = (getMovieListHtml(item))
+            console.log(movieArrayHtml)
+        });
+        movieResults.innerHTML += movieArrayHtml
+    }
 }
 
-function getMovieListHtml(poster, title, rating, runtime, genre, plot) {
+function getMovieListHtml(movie) {
+    const { Poster, Title, imdbRating, Runtime, Genre, Plot } = movie
+    movieResults.style.background = 'none'
     return html = `
         <div class="item">
-            <img src="${poster}">
+            <img src="${Poster}" alt=${Title}>
             <div>
                 <div class="item-header">
-                    <h2>${title}</h2>
-                    <p class="small-text-400">⭐️ ${rating}</p>
+                    <h2>${Title}</h2>
+                    <p class="small-text-400">⭐️ ${imdbRating}</p>
                 </div>
                 <div class="item-details">
-                    <p class="small-text-400">${runtime}</p>
-                    <p class="small-text-400">${genre}</p>
+                    <p class="small-text-400">${Runtime}</p>
+                    <p class="small-text-400">${Genre}</p>
                     <div class="item-watchlist">
                         <button id="add-to-watchlist-btn" class="add-to-watchlist-btn">
                             <img src="images/icon-1.png" alt="add to watchlist button">
@@ -45,11 +59,7 @@ function getMovieListHtml(poster, title, rating, runtime, genre, plot) {
                         <p class="small-text-400">Watchlist</p>
                     </div>
                 </div>
-                <p class="plot smaller-text-400">${plot.slice(0, 125) + `<span>... Read More</span>`}</p>
+                <p class="plot smaller-text-400">${Plot}</p>
             </div>
         </div>`
-}
-
-function renderHtml() {
-    movieResults.innerHTML = html
 }
