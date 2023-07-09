69feab746e9bd6931bb66df349cfe62dbf3b3d2a
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-07-09 08:32:46 +0200

    update project

--- movie-watchlist/index.js
+++ movie-watchlist/index.js
@@ -7,11 +7,10 @@ let html = ''
 let movieArrayHtml = ''
 let movieArray = []
 
-
-searchInput.addEventListener('change', (e) => {
-    searchTerm = searchInput.value
-    // searchInput.value = ""
-    return searchTerm
+searchInput.addEventListener('change', () => {
+        searchTerm = searchInput.value
+        searchInput.value = ""
+        return searchTerm
 })
 
 searchBtn.addEventListener('click', async (e) => {
@@ -22,24 +21,57 @@ searchBtn.addEventListener('click', async (e) => {
 async function getMovie(movieTitle) {
     const movieResponse = await fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=ead28c58&t`)
     const data = await movieResponse.json()
-    // console.log(data.Search[0])
+    console.log(data.Search[0])
 
     for (let i = 0; i < data.Search.length; i++) {
         const imdbIdResponse = await fetch(`http://www.omdbapi.com/?i=${data.Search[i].imdbID}&apikey=ead28c58&t`)
         const imdbIdData = await imdbIdResponse.json()
-        // console.log(imdbIdData)
+        console.log(imdbIdData)
         movieArray.push(imdbIdData)
         // console.log(movieArray)
         movieArray.forEach(item => {
             movieArrayHtml = (getMovieListHtml(item))
-            console.log(movieArrayHtml)
+            // console.log(movieArrayHtml)
         });
         movieResults.innerHTML += movieArrayHtml
+
+        const watchlistBtn = document.getElementById('add-to-watchlist-btn')
+
+        watchlistBtn.addEventListener('click', (e) => {
+            if (e.target.dataset.imdbId) {
+                let movie = {
+                    "imdbID": imdbIdData.imdbID,
+                    "Poster": imdbIdData.Poster,
+                    "Title": imdbIdData.Title,
+                    "imdbRating": imdbIdData.imdbRating,
+                    "Runtime": imdbIdData.Runtime,
+                    "Genre": imdbIdData.Genre,
+                    "Plot": imdbIdData.Plot,
+                }
+                // console.log(movie)
+
+                /* TODO: figure out how to get watchlistBtn to be clickable for every movie */
+
+                if (localStorage.getItem('movies')) {
+                    let watchlistArray = JSON.parse(localStorage.getItem('movies'))
+                    watchlistArray.push(movie)
+                    localStorage.setItem('movies', JSON.stringify(watchlistArray))
+                    console.log('IF:', watchlistArray)
+                }
+                else {
+                    let watchlistArray = []
+                    watchlistArray.push(movie)
+                    localStorage.setItem('movies', JSON.stringify(watchlistArray))
+                    console.log('ELSE:', watchlistArray)
+                }
+
+            }
+        })           
     }
 }
 
 function getMovieListHtml(movie) {
-    const { Poster, Title, imdbRating, Runtime, Genre, Plot } = movie
+    const { Poster, Title, imdbRating, Runtime, Genre, imdbID, Plot } = movie
     movieResults.style.background = 'none'
     return html = `
         <div class="item">
@@ -54,7 +86,7 @@ function getMovieListHtml(movie) {
                     <p class="small-text-400">${Genre}</p>
                     <div class="item-watchlist">
                         <button id="add-to-watchlist-btn" class="add-to-watchlist-btn">
-                            <img src="images/icon-1.png" alt="add to watchlist button">
+                            <img data-imdb-id="${imdbID}" src="images/icon-1.png" alt="add to watchlist button">
                         </button>
                         <p class="small-text-400">Watchlist</p>
                     </div>
