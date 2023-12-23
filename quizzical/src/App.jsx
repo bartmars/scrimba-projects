893bd8fa162ca9412e5e5cf13a94328e211d1486
Bart Mars <bart.mars@outlook.com>
2023-12-23 17:40:24 +0100

    update project

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -2,13 +2,20 @@
 
 // eslint-disable-next-line no-unused-vars
 import React from "react"
+import blobTopRight from "./assets/blob-top-right.png"
+import blobBottomLeft from "./assets/blob-bottom-left.png"
 
 export default function App() {
   return (
-    <main>
-      <h1 className="title">Quizzical</h1>
-      <p className="description">Click the button below to start the game</p>
-      <button className="btn">Start</button>
-    </main>
+    <div className="content">
+      <img src={blobTopRight} className="blob-top-right"/>
+      <div className="container">
+        <h1 className="title">Quizzical</h1>
+        <p className="description">Click the button below to start the game</p>
+        <button className="btn">Start quiz</button>
+      </div>
+      <img src={blobBottomLeft} className="blob-bottom-left"/>
+    </div>
+
   )
 } 
