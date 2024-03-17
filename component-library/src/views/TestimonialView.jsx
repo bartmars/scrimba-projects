5f7b2326ec2718da447fabaa1cf4c7b3eb404a12
Bart Mars <bart.mars@outlook.com>
2024-03-17 20:05:51 +0100

    update project

--- component-library/src/views/TestimonialView.jsx
+++ component-library/src/views/TestimonialView.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Testimonial from "../components/Testimonial/index"
-// import testimonialImage from "../assets/testimonialImage.png"
-import image from "../assets/render-male.png"
+import testimonialImage from "../assets/testimonialImage.png"
+// import image from "../assets/render-male.png"
+import logo from "../assets/logo.png"
 
 export default function TestimonialView() {
     return (
         <>
-            <Testimonial color="dark-blue" image={image} >
+            <Testimonial color="dark-blue" image={testimonialImage} logo={logo} >
                 <Testimonial.Image/>
                 <Testimonial.Text>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
